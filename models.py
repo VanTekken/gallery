0000bc64657d1d0f5e56ca74b7d74bb012e9b5a8
vantekken <harry.crawford789@hotmail.co.uk>
2018-11-07 10:55:07 +0000

    Implementing a default theme

--- models.py
+++ models.py
@@ -10,18 +10,25 @@ class Images(models.Model):
 	def __str__(self):
 		return self.image_text
 
+class ThemesManager(models.Manager):
+	def create_theme(self, nme, url, sld):
+		theme = Themes(nme=nme,url=url,sld=sld)
+		return theme
+
 class Themes(models.Model):
 	#selection = models.ForeignKey('BackgroundSelection', on_delete=models.CASCADE)
 	nme = models.CharField(max_length=200)
 	url = models.CharField(max_length=200)
 	sld = models.BooleanField(default=False)
 
+	objects = ThemesManager()
 	def __str__(self):
 		return self.nme
 
 	#SELECTION = models.ForeignKey('Backgrounds', on_delete=models.CASCADE)
 	#BACKGROUND_OPTS = ((template_url, background_name),)
 
+theme = Themes.objects.create_theme("Particles-js","gallery/particles.html",True)
 
 
 '''
